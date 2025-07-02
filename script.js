ba183410140088494b4c8dfdfced700e48f8b3ed
Haru-143 <143gaeulpink@gmail.com>
2025-07-02 15:07:02 +0800

    Add operate function that takes an operator and two numbers and calls one of the 4 basic operator function

---
+++ script.js
@@ -0,0 +1,30 @@
+function addition(num1, num2) {
+    return num1 + num2;
+}
+
+function subtraction(num1, num2) {
+    return num1 - num2;
+}
+
+function multiplication(num1, num2) {
+    return num1 * num2;
+}
+
+function division(num1, num2) {
+    return num1 / num2;
+}
+
+function operate(operator, num1, num2) {
+    switch(operator) {
+        case '+':
+            return addition(num1, num2);
+        case '-':
+            return subtraction(num1, num2);
+        case '*':
+            return multiplication(num1, num2);
+        case '/':
+            return division(num1, num2);
+        default:
+            throw new Error('Please enter a valid operator');
+    }
+}
