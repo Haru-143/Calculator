ad878cc460faff036a4664e1f1693754ebf23071
Haru-143 <143gaeulpink@gmail.com>
2025-07-05 15:28:35 +0800

    Add alert in division function when dividing by 0

--- script.js
+++ script.js
@@ -11,20 +11,8 @@ function multiplication(num1, num2) {
 }
 
 function division(num1, num2) {
-    return num1 / num2;
-}
-
-function operate(operator, num1, num2) {
-    switch(operator) {
-        case '+':
-            return addition(num1, num2);
-        case '-':
-            return subtraction(num1, num2);
-        case '*':
-            return multiplication(num1, num2);
-        case '/':
-            return division(num1, num2);
-        default:
-            throw new Error('Please enter a valid operator');
+    if (num2 === 0) {
+        alert("You can't divide by 0 duhhh!!!");
     }
+    return num1 / num2;
 }
